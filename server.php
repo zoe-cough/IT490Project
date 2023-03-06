0ac9008abf09cbef12eba54c10014bd80b2b9fdc
MarcusNW <mnw8@njit.edu>
2023-03-06 12:30:52 -0500

    The working authentication webpage

--- server.php
+++ server.php
@@ -7,7 +7,7 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'registration');
+$db = mysqli_connect('127.0.0.1', 'testuser', 'Password135$', 'registration');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
